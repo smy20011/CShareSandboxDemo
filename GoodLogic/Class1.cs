9e1fb024bd15828e708eab955a8edb97ff0e5cbf
smy20011 <smy20011@gmail.com>
2015-02-20 04:12:21 -0600

    Add Code for good/untested project

---
+++ GoodLogic/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SandboxTest
+{
+    public class SandboxTest
+    {
+        static public int add(int a, int b)
+        {
+            return a + b;
+        }
+
+        static public int minus(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
---
+++ UntestedLogic/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace SandboxTest
+{
+    public class SandboxTest
+    {
+        static public int add(int a, int b)
+        {
+            // Something unintended happens
+            File.ReadAllText("C:\\Temp\\file.txt");
+            return a + b;
+        }
+
+        static public int minus(int a, int b)
+        {
+            // And exception
+            throw new ArgumentException();
+            return a - b;
+        }
+    }
+}
